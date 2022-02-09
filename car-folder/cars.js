2622c1cd5848c5e316b84a2e5b6d281f978f0c3a
BrendenSeifried <brendenseifried@gmail.com>
2022-02-08 17:39:42 -0800

    cleaned up code

--- car-folder/cars.js
+++ car-folder/cars.js
@@ -1,22 +1,7 @@
-// import { cars } from '../data.js';
-// import { findById } from '../utils.js';
 
-// const dataStuff = new URLSearchParams(window.location.search);
-// const car = findById(dataStuff.get('id'), cars);
-// const anImg = document.getElementById('img');
-
-// const make = document.getElementById('name');
-// const stats = document.getElementById('stats');
-
-// make.textContent = car.make;
-// stats.textContent = car.stats;
-// anImg.src = `../assets/${car.img}.png`;
-
-  
 import { cars } from '../data.js';
 import { findById } from '../utils.js';
-//import { renderCar } from '../render-utils.js';
-//const carList = document.getElementById('car');
+
 
 
 const dataStuff = new URLSearchParams(window.location.search);
@@ -35,13 +20,4 @@ stats.textContent = car.stats;
 anImg.src = `../assets/${car.img}.png`;
 
 
-
-// function renderCars() { 
-    
-//     for (let car of cars) {
-//         const div = renderCar(car);
-//         carList.append(div);
-//     }
-// }
-// renderCars();
 
--- test/data.test.js
+++ test/data.test.js
@@ -31,17 +31,3 @@ test('renderCars test', (expect) => {
 
 
 
-
-// test('time to test a function', (expect) => {
-//     //Arrange
-//     // Set up your arguments and expectations
-//     const expected = true;
-    
-//     //Act 
-//     // Call the function you're testing and set the result to a const
-//     const actual = true;
-
-//     //Expect
-//     // Make assertions about what is expected versus the actual result
-//     expect.equal(actual, expected);
-// });
